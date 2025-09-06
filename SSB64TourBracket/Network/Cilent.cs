1a6d17227ea72e7c09667bf87b3a64cca328ab0d
SmithJrBlaquaLuigi <damon0959@gmail.com>
2025-09-06 00:56:26 -0400

    Cilent moved to Network folder as a separation.

---
+++ SSB64TourBracket/Network/Cilent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.WebSockets;
+using System.Threading;
+
+namespace SSB64TournamentBracket
+{
+    internal class Cilent
+    {
+
+    }
+}
